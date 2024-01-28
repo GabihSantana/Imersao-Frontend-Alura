3af410672259e74db3126d692d8ea14276dc9212
Gabriela <gabriela2santtanna@gmail.com>
2024-01-27 21:32:54 -0300

    Organizando

--- spotify-imersao-alura/README.md
+++
@@ -1,2 +0,0 @@
-Repositório dedicado à semana de Imersão no Front-end da Alura!!
-Estamos desenvolvendo uma cópia do landing page do Spotify!!!
--- spotify-imersao-alura/src/App.js
+++
@@ -1,8 +0,0 @@
-import "./styles.css";
-import Header from "./header/header.js";
-
-export function App() {
-  return <header />;
-}
-
-export default Header;
